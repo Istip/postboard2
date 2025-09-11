34fc36f8342d48d4fe862e8a48809ef05189d9f5
Pasztor Isti <isticsek@gmail.com>
2025-09-11 19:41:13 +0300

    updating basic fonts and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <h1 className="heading">Hello to the App</h1>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
+      distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
+      aspernatur ab explicabo labore facilis enim praesentium expedita quia
+      atque.
+    </div>
+  );
 };
 
 export default App;

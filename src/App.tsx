5f3051c2931a97554a6aaf626e088f35900f346b
Pasztor Isti <isticsek@gmail.com>
2025-09-11 22:56:35 +0300

    class testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       App
-      <h1 className="heading">Hello to the App</h1>
+      <h1 className="heading text-4xl">Hello to the App</h1>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
       distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
       aspernatur ab explicabo labore facilis enim praesentium expedita quia

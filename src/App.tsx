62fa8abdbb88fd1861bdf7c2c5960ccc21cd0385
Pasztor Isti <isticsek@gmail.com>
2025-09-13 20:57:57 +0300

    testing button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
   return (
     <div>
       App
       <h1 className="heading text-4xl">Hello to the App</h1>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
-      distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
-      aspernatur ab explicabo labore facilis enim praesentium expedita quia
-      atque.
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
+        distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
+        aspernatur ab explicabo labore facilis enim praesentium expedita quia
+        atque.
+      </p>
+      <Button>Click me</Button>
     </div>
   );
 };

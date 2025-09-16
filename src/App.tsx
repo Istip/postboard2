5c5b73e0429cf172616d3c618b552223e0f8ddb1
Pasztor Isti <isticsek@gmail.com>
2025-09-16 19:57:10 +0300

    minimal updates for testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
-    <div>
-      App
-      <h1 className="heading text-4xl">Hello to the App</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
-        distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
-        aspernatur ab explicabo labore facilis enim praesentium expedita quia
-        atque.
-      </p>
-      <Button>Click me</Button>
+    <div className="center">
+      <div className="container p-4">
+        <h1 className="heading text-4xl text-center">Postboard</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
+          distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
+          aspernatur ab explicabo labore facilis enim praesentium expedita quia
+          atque.
+        </p>
+        <Button>AUTHENTICATE</Button>
+      </div>
     </div>
   );
 };

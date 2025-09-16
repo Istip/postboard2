dc2e823bcb63a605396bc7f92782285c556c59f8
Pasztor Isti <isticsek@gmail.com>
2025-09-16 21:07:36 +0300

    update main app (manily) with theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/Theme/ThemeProvider";
+import { ThemeToggle } from "@/components/Theme/ThemeToggle";
 
-const App = () => {
+export default function App() {
   return (
-    <div className="center">
-      <div className="container p-4">
-        <h1 className="heading text-4xl text-center">Postboard</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. In repellat,
-          distinctio earum quaerat eius qui sit iste consequatur ullam adipisci
-          aspernatur ab explicabo labore facilis enim praesentium expedita quia
-          atque.
-        </p>
-        <Button>AUTHENTICATE</Button>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeToggle />
+      <div className="center">
+        <div className="container p-4">
+          <h1 className="heading text-4xl text-center">Postboard</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+            repellat, distinctio earum quaerat eius qui sit iste consequatur
+            ullam adipisci aspernatur ab explicabo labore facilis enim
+            praesentium expedita quia atque.
+          </p>
+          <Button>AUTHENTICATE</Button>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
-};
-
-export default App;
+}

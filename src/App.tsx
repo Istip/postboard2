f25efc54e9782ba1cacb1661f8ae3979d58a24cf
Pasztor Isti <isticsek@gmail.com>
2025-09-18 23:53:44 +0300

    remove user fetching test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useAuthStore } from "@/stores/auth.store";
 import { ThemeProvider } from "@/components/Theme/ThemeProvider";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import DashboardLayout from "@/layouts/DashboardLayout";
@@ -6,6 +8,12 @@ import Dashboard from "@/pages/Dashboard";
 import Home from "@/pages/Home";
 
 export default function App() {
+  const fetchUser = useAuthStore((state) => state.fetchUser);
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>

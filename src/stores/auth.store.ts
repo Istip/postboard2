1ffd28939bc40d3d1eb0298261542e778df4ab3c
Pasztor Isti <isticsek@gmail.com>
2025-09-18 12:46:30 +0300

    create auth store for zustand

---
+++ src/stores/auth.store.ts
@@ -0,0 +1,77 @@
+import { account } from "@/lib/appwrite";
+import { type Models } from "appwrite";
+import { create } from "zustand";
+
+type AuthState = {
+  user: Models.User | null;
+  loading: boolean;
+  error: string | null;
+  login: (email: string, password: string) => Promise<Models.User | null>;
+  register: (
+    id: string,
+    email: string,
+    password: string,
+    name: string
+  ) => Promise<Models.User | null>;
+  logout: () => Promise<void>;
+  fetchUser: () => Promise<Models.User | null>;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  loading: false,
+  error: null,
+
+  register: async (
+    id: string,
+    email: string,
+    password: string,
+    name: string
+  ) => {
+    set({ loading: true, error: null });
+    try {
+      const user = await account.create(id, email, password, name);
+      set({ user, loading: false });
+      return user;
+    } catch (error) {
+      console.error("Registration failed: ", JSON.stringify(error, null, 2));
+      set({ error: (error as Error).message, loading: false });
+      return null;
+    }
+  },
+
+  login: async (email: string, password: string) => {
+    set({ loading: true, error: null });
+    try {
+      await account.createEmailPasswordSession(email, password);
+      const user = await account.get();
+      set({ user, loading: false });
+      return user;
+    } catch (error) {
+      set({ error: (error as Error).message, loading: false });
+      return null;
+    }
+  },
+
+  logout: async () => {
+    set({ loading: true, error: null });
+    try {
+      await account.deleteSession("current");
+      set({ user: null, loading: false });
+    } catch (error) {
+      set({ error: (error as Error).message, loading: false });
+    }
+  },
+
+  fetchUser: async () => {
+    set({ loading: true, error: null });
+    try {
+      const user = await account.get();
+      set({ user, loading: false });
+      return user;
+    } catch (error) {
+      set({ user: null, error: null, loading: false });
+      return null;
+    }
+  },
+}));

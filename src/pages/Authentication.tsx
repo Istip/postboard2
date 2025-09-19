f27372066deba55f303d40e8211281587b077b49
Pasztor Isti <isticsek@gmail.com>
2025-09-19 20:28:00 +0300

    add some auth stuff

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,34 +1,24 @@
-import { Button } from "@/components/ui/button";
-import { useAuthStore } from "@/stores/auth.store";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Register from "@/components/Authentication/Register";
+import Login from "@/components/Authentication/Login";
 
 const Authentication = () => {
-  const user = useAuthStore((state) => state.user);
-  const loading = useAuthStore((state) => state.loading);
-  const login = useAuthStore((state) => state.login);
-  const logout = useAuthStore((state) => state.logout);
-
-  const handleLogin = async () => {
-    login("", "");
-  };
-
-  const handleLogout = async () => {
-    logout();
-  };
-
   return (
-    <div>
-      <Button disabled={loading} variant="secondary" onClick={handleLogin}>
-        LOGIN
-      </Button>
-      <Button disabled={loading} variant="destructive" onClick={handleLogout}>
-        LOGOUT
-      </Button>
-      <code>
-        <pre>{user && JSON.stringify(user, null, 2)}</pre>
-      </code>
-      <Register />
-    </div>
+    <>
+      <p className="text-center pb-6">Yo mate, welcome!</p>
+      <Tabs defaultValue="login">
+        <TabsList className="w-full mb-6">
+          <TabsTrigger value="login">LOGIN</TabsTrigger>
+          <TabsTrigger value="register">REGISTER</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <Login />
+        </TabsContent>
+        <TabsContent value="register">
+          <Register />
+        </TabsContent>
+      </Tabs>
+    </>
   );
 };
 

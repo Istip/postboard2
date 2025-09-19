7d650eb884f6629ec04ea273173c4654d09c8ce6
Pasztor Isti <isticsek@gmail.com>
2025-09-19 20:50:12 +0300

    add test logout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { useAuthStore } from "@/stores/auth.store";
 
 const Home = () => {
   const user = useAuthStore((state) => state.user);
+  const logout = useAuthStore((state) => state.logout);
 
   return (
     <div>
       Hello <b>{user ? user.name : "Guest"}</b>
+      <br />
+      <br />
+      <br />
+      <Button variant="outline" onClick={logout}>
+        Logout
+      </Button>
     </div>
   );
 };

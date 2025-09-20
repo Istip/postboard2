2c11686b69022dd6c3afcb242ee3767d6f2bf288
Pasztor Isti <isticsek@gmail.com>
2025-09-20 18:12:28 +0300

    remove yo text

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -51,25 +51,22 @@ const Authentication = () => {
   }
 
   return (
-    <>
-      <p className="text-center pb-6">Yo mate, welcome!</p>
-      <Tabs defaultValue="login">
-        <TabsList className="w-full mb-6">
-          <TabsTrigger className="cursor-pointer" value="login">
-            LOGIN
-          </TabsTrigger>
-          <TabsTrigger className="cursor-pointer" value="register">
-            REGISTER
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="login">
-          <Login />
-        </TabsContent>
-        <TabsContent value="register">
-          <Register />
-        </TabsContent>
-      </Tabs>
-    </>
+    <Tabs defaultValue="login">
+      <TabsList className="w-full mb-6">
+        <TabsTrigger className="cursor-pointer" value="login">
+          LOGIN
+        </TabsTrigger>
+        <TabsTrigger className="cursor-pointer" value="register">
+          REGISTER
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="login">
+        <Login />
+      </TabsContent>
+      <TabsContent value="register">
+        <Register />
+      </TabsContent>
+    </Tabs>
   );
 };
 

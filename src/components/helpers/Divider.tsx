b4202ed78b4f2fdb41a30aacf9046ed8a2daef82
Pasztor Isti <isticsek@gmail.com>
2025-09-22 00:18:59 +0300

    create divider component

--- src/components/helpers/Divider.tsx
+++ src/components/helpers/Divider.tsx
@@ -2,13 +2,13 @@ const Divider = ({ children }: { children?: string }) => {
   if (children && children.length > 2) {
     return (
       <div className="center gap-2">
-        <div className="h-[1px] w-full bg-secondary rounded-full my-2"></div>
+        <div className="h-[1px] w-full bg-secondary rounded-full my-4"></div>
         <div className="text-[10px] text-primary">{children}</div>
-        <div className="h-[1px] w-full bg-secondary rounded-full my-2"></div>
+        <div className="h-[1px] w-full bg-secondary rounded-full my-4"></div>
       </div>
     );
   }
-  return <div className="h-[1px] w-full bg-secondary rounded-full my-2" />;
+  return <div className="h-[1px] w-full bg-secondary rounded-full my-4" />;
 };
 
 export default Divider;

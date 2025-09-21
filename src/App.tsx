cd986ecf8ee00ec52b6d4c7b5dfd7147648cce20
Pasztor Isti <isticsek@gmail.com>
2025-09-21 19:48:00 +0300

    change route system

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,26 @@ import { ThemeProvider } from "@/components/Theme/ThemeProvider";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import Authentication from "@/pages/Authentication";
-import Dashboard from "@/pages/Dashboard";
 import Home from "@/pages/Home";
 
 export default function App() {
   const fetchUser = useAuthStore((state) => state.fetchUser);
+  const loading = useAuthStore((state) => state.loading);
 
   useEffect(() => {
     fetchUser();
   }, [fetchUser]);
 
+  // TODO: add loading
+  if (loading) return null;
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
         <Routes>
-          <Route index element={<Home />} />
           <Route path="auth" element={<Authentication />} />
           <Route element={<DashboardLayout />}>
-            <Route path="dashboard" element={<Dashboard />} />
+            <Route index element={<Home />} />
           </Route>
         </Routes>
       </Router>

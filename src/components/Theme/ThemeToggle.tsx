2c1c9c6d41eb3b55b379fba534445f49353e43a2
Pasztor Isti <isticsek@gmail.com>
2025-09-21 21:32:15 +0300

    make theme toggler waaaay fancier <3

--- src/components/Theme/ThemeToggle.tsx
+++ src/components/Theme/ThemeToggle.tsx
@@ -1,62 +1,50 @@
-import { Moon, Sun, Computer } from "lucide-react";
+import { Moon, Sun, SunMoon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { useTheme } from "@/components/Theme/ThemeProvider";
 import ScreenReader from "@/components/Helpers/ScreenReader";
 
 type Theme = {
   name: "light" | "dark" | "system";
-  icon: "Sun" | "Moon" | "Computer";
+  icon: "Sun" | "Moon" | "SunMoon";
 };
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
-  const iconMap = { Sun, Moon, Computer };
+  const iconMap = { Sun, Moon, SunMoon };
 
   const themes: Theme[] = [
     { name: "light", icon: "Sun" },
     { name: "dark", icon: "Moon" },
-    { name: "system", icon: "Computer" },
+    { name: "system", icon: "SunMoon" },
   ];
 
   const TogglerIcon = ({ name }: { name: string }) => {
     const IconComponent = iconMap[name as keyof typeof iconMap];
-    return <IconComponent strokeWidth={1} className="mr-2 h-4 w-4" />;
+    return (
+      <div className="flex justify-center items-center text-center">
+        <IconComponent strokeWidth={1} className="h-4 w-4 " />
+      </div>
+    );
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className="rounded-full">
-        <Button variant="default" size="icon">
-          <Sun
-            strokeWidth={1}
-            className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
-          />
-          <Moon
-            strokeWidth={1}
-            className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
-          />
-          <ScreenReader>Toggle theme</ScreenReader>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+    <div className="border bg-card p-2 rounded-sm mb-2">
+      <h2 className="text-center pb-2">Theme selector</h2>
+      <div className="center gap-2">
         {themes.map(({ name, icon }) => (
-          <DropdownMenuItem
+          <Button
             key={name}
             onClick={() => setTheme(name)}
             className="cursor-pointer capitalize"
+            size="icon"
+            variant={theme === name ? "default" : "secondary"}
           >
             <TogglerIcon name={icon} />
-            <span>{name}</span>
-          </DropdownMenuItem>
+            <ScreenReader>{name}</ScreenReader>
+          </Button>
         ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </div>
+    </div>
   );
 }

625038ca2520219bca158308467c320918197edd
Pasztor Isti <isticsek@gmail.com>
2025-09-21 21:41:38 +0300

    remove test login from home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,11 @@
-import { Button } from "@/components/ui/button";
 import { useAuthStore } from "@/stores/auth.store";
 
 const Home = () => {
   const user = useAuthStore((state) => state.user);
-  const logout = useAuthStore((state) => state.logout);
 
   return (
     <>
       Hello <b>{user ? user.name : "Guest"}</b>
-      <br />
-      <br />
-      <br />
-      <Button variant="outline" onClick={logout}>
-        Logout
-      </Button>
     </>
   );
 };

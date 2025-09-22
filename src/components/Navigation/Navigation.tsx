07f24a16eedfeff2cc8e380acce14e41e4019b83
Pasztor Isti <isticsek@gmail.com>
2025-09-22 10:41:57 +0300

    clean up navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,7 @@
 import { useAuthStore } from "@/stores/auth.store";
-import { useNavigate } from "react-router";
 import { ThemeToggle } from "@/components/Theme/ThemeToggle";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { initials } from "@/lib/initials";
-import { Button } from "@/components/ui/button";
 import Divider from "@/components/Helpers/Divider";
 import {
   Tooltip,
@@ -16,70 +14,45 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Logo from "@/components/Navigation/Logo";
 
 const Navigation = () => {
   const user = useAuthStore((state) => state.user);
   const logout = useAuthStore((state) => state.logout);
 
-  const navigate = useNavigate();
-
-  const handleNavigateHome = () => navigate("/");
-
   return (
-    <nav className="border-b border-secondary p-4 flex items-center justify-between shadow-primary shadow-xs transition-all delay-200 lg:hover:shadow-md">
-      <div className="h-4 w-4 rounded-full bg-primary">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="h-4 w-4 p-0"
-          onClick={handleNavigateHome}
-        />
-      </div>
-      <div className="center">
-        {user && (
-          <DropdownMenu>
-            <Tooltip>
-              <TooltipTrigger>
-                <DropdownMenuTrigger asChild>
-                  <Avatar className="cursor-pointer">
-                    <AvatarImage src="" alt="User Avatar" />
-                    <AvatarFallback>{initials(user!.name)}</AvatarFallback>
-                  </Avatar>
-                </DropdownMenuTrigger>
-              </TooltipTrigger>
-              <TooltipContent>Open the Profile Menu</TooltipContent>
-            </Tooltip>
-            <DropdownMenuContent className="p-2 mr-2">
-              <div className="center flex-col">
-                <h2 className="text-xl">Hello,</h2>
-                <p className="font-black">{user?.name}</p>
-              </div>
-              <Divider />
-              <DropdownMenuItem>Home</DropdownMenuItem>
-              <Divider>SETTINGS</Divider>
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <Divider />
-              <ThemeToggle />
-              <Divider />
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <DropdownMenuItem
-                    variant="destructive"
-                    onClick={logout}
-                    className="text-center center"
-                  >
-                    Logout
-                  </DropdownMenuItem>
-                </TooltipTrigger>
-                <TooltipContent>
-                  Logging out? Why would you do that?
-                </TooltipContent>
-              </Tooltip>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
+    <nav className="border-b border-secondary p-4 flex items-center justify-between shadow-primary shadow-xs transition-all duration-200 lg:hover:shadow-md">
+      <Logo />
+      {user && (
+        <DropdownMenu>
+          <Tooltip>
+            <TooltipTrigger>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="" alt="User Avatar" />
+                  <AvatarFallback>{initials(user!.name)}</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+            </TooltipTrigger>
+            <TooltipContent>Open the Profile Menu</TooltipContent>
+          </Tooltip>
+
+          <DropdownMenuContent className="p-2 mr-2 text-center">
+            <h2 className="text-xl">Hello,</h2>
+            <p className="font-black">{user?.name}</p>
+            <Divider />
+            <DropdownMenuItem>Home</DropdownMenuItem>
+            <Divider>SETTINGS</Divider>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <Divider />
+            <ThemeToggle />
+            <DropdownMenuItem variant="destructive" onClick={logout}>
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </nav>
   );
 };

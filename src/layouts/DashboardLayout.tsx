57bc709bcd685d2a13e8a1de38f7973c28f3cfd3
Pasztor Isti <isticsek@gmail.com>
2025-09-23 22:05:29 +0300

    fix dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import Navigation from "@/components/Navigation/Navigation";
 import { Outlet } from "react-router";
 
@@ -5,9 +6,10 @@ const DashboardLayout = () => {
   return (
     <>
       <Navigation />
-      <div className="p-4">
+      <div className="p-4 min-h-[calc(100dvh-71px)] pb-[71px]">
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 };

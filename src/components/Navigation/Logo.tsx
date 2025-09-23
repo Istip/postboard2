22ae1d7968c7be3cf5ec877f26f4980ba593609d
Pasztor Isti <isticsek@gmail.com>
2025-09-23 22:02:16 +0300

    update logo with actual app name

--- src/components/Navigation/Logo.tsx
+++ src/components/Navigation/Logo.tsx
@@ -1,19 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
 const Logo = () => {
-  const navigate = useNavigate();
-  const handleNavigateHome = () => navigate("/");
-
   return (
-    <div className="h-4 w-4 rounded-full bg-primary">
-      <Button
-        variant="ghost"
-        size="icon"
-        className="h-4 w-4 p-0"
-        onClick={handleNavigateHome}
-      />
-    </div>
+    <Link to="/" className="space-x-2 center">
+      <div className="rounded-full w-4 h-4 bg-primary" />
+      <p className="heading text-primary">Postboard</p>
+    </Link>
   );
 };
 

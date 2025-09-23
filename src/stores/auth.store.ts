21cc921f534c0c010b34883d63b4a437e46af3b3
Pasztor Isti <isticsek@gmail.com>
2025-09-23 20:42:13 +0300

    adding persist for the local storage

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,6 +1,7 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { account } from "@/lib/appwrite";
 import { type Models } from "appwrite";
-import { create } from "zustand";
 
 type AuthState = {
   user: Models.User | null;
@@ -17,60 +18,68 @@ type AuthState = {
   fetchUser: () => Promise<Models.User | null>;
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  loading: false,
-  error: null,
+export const useAuthStore = create(
+  persist<AuthState>(
+    (set) => ({
+      user: null,
+      loading: false,
+      error: null,
 
-  register: async (
-    id: string,
-    email: string,
-    password: string,
-    name: string
-  ) => {
-    set({ loading: true, error: null });
-    try {
-      const user = await account.create(id, email, password, name);
-      set({ user, loading: false });
-      return user;
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-      return null;
-    }
-  },
+      register: async (
+        id: string,
+        email: string,
+        password: string,
+        name: string
+      ) => {
+        set({ loading: true, error: null });
+        try {
+          const user = await account.create(id, email, password, name);
+          set({ user, loading: false });
+          return user;
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+          return null;
+        }
+      },
 
-  login: async (email: string, password: string) => {
-    set({ loading: true, error: null });
-    try {
-      await account.createEmailPasswordSession(email, password);
-      const user = await account.get();
-      set({ user, loading: false });
-      return user;
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-      return null;
-    }
-  },
+      login: async (email: string, password: string) => {
+        set({ loading: true, error: null });
+        try {
+          await account.createEmailPasswordSession(email, password);
+          const user = await account.get();
+          set({ user, loading: false });
+          return user;
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+          return null;
+        }
+      },
 
-  logout: async () => {
-    set({ loading: true, error: null });
-    try {
-      await account.deleteSession("current");
-      set({ user: null, loading: false });
-    } catch (error) {
-      set({ error: (error as Error).message, loading: false });
-    }
-  },
+      logout: async () => {
+        set({ loading: true, error: null });
+        try {
+          await account.deleteSession("current");
+          set({ user: null, loading: false });
+        } catch (error) {
+          set({ error: (error as Error).message, loading: false });
+        }
+      },
 
-  fetchUser: async () => {
-    set({ loading: true, error: null });
-    try {
-      const user = await account.get();
-      set({ user, loading: false });
-      return user;
-    } catch (error) {
-      set({ user: null, error: null, loading: false });
-      return null;
+      fetchUser: async () => {
+        set({ loading: true, error: null });
+        try {
+          const user = await account.get();
+          set({ user, loading: false });
+          return user;
+        } catch (error) {
+          set({ user: null, error: null, loading: false });
+          return null;
+        }
+      },
+    }),
+    {
+      name: "auth-store",
+      storage: createJSONStorage(() => localStorage),
     }
-  },
-}));
+  )
+);

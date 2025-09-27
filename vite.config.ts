3ad2bf8c3ffd32a6c69767d3492f2de1e1586c04
Pasztor Isti <isticsek@gmail.com>
2025-09-27 16:05:21 +0300

    create pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestIcons = [
+  {
+    src: "192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+];
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Postboard - Manage your stuff efficiently",
+        short_name: "Postboard",
+        icons: manifestIcons,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),

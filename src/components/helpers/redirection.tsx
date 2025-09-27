1fd1042264943114690379042855c6a964a6401f
Pasztor Isti <isticsek@gmail.com>
2025-09-27 15:22:03 +0300

    extract redirection to a separate component

---
+++ src/components/helpers/redirection.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/stores/auth.store";
+import { Home } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router";
+
+const Redirection = () => {
+  const time = 5;
+  const [counter, setCounter] = useState(time);
+  const user = useAuthStore((state) => state.user);
+  const navigate = useNavigate();
+
+  // Start countdown when user is present
+  useEffect(() => {
+    if (!user) return;
+    setCounter(time);
+    const interval = setInterval(() => {
+      setCounter((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [user]);
+
+  // Navigate to the home page when counter reaches 0
+  useEffect(() => {
+    if (user && counter <= 0) {
+      navigate("/");
+    }
+  }, [user, counter, navigate]);
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-4 w-screen h-[100dvh] text-center">
+      <div>
+        <p>
+          You are already logged in as{" "}
+          <b className="text-primary">{user!.name}</b>
+        </p>
+        <p>
+          Redirecting to the Dashboard in{" "}
+          <b className="text-primary">{counter}</b> seconds...
+        </p>
+      </div>
+
+      <Button asChild variant="default">
+        <Link to="/">
+          <Home />
+          Go to Dashboard
+        </Link>
+      </Button>
+    </div>
+  );
+};
+
+export default Redirection;
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,63 +1,14 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router";
 import { useAuthStore } from "@/stores/auth.store";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react";
 import Login from "@/components/authentication/login";
 import Register from "@/components/authentication/register";
+import Redirection from "@/components/helpers/redirection";
 
 const Authentication = () => {
-  const time = 5;
-
-  const [counter, setCounter] = useState(time);
   const user = useAuthStore((state) => state.user);
-  const loading = useAuthStore((state) => state.loading);
-
-  const navigate = useNavigate();
-
-  // Start countdown when user is present
-  useEffect(() => {
-    if (!user) return;
-    setCounter(time);
-    const interval = setInterval(() => {
-      setCounter((prev) => prev - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [user]);
-
-  // Navigate when counter reaches 0
-  useEffect(() => {
-    if (user && counter <= 0) {
-      navigate("/");
-    }
-  }, [user, counter, navigate]);
-
-  if (loading) return null;
 
   if (user) {
-    return (
-      <div className="flex flex-col justify-center items-center gap-4 w-screen h-[100dvh]">
-        <div className="text-center">
-          <p>
-            You are already logged in as{" "}
-            <b className="text-primary">{user.name}</b>
-          </p>
-          <p>
-            Redirecting to the Dashboard in{" "}
-            <b className="text-primary">{counter}</b> seconds...
-          </p>
-        </div>
-
-        <Button asChild variant="default">
-          <Link to="/">
-            <Home />
-            Go to Dashboard
-          </Link>
-        </Button>
-      </div>
-    );
+    return <Redirection />;
   }
 
   return (

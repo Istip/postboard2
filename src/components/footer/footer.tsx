350df8325be253836fa3645426eb7a58430cdc9b
Pasztor Isti <isticsek@gmail.com>
2025-09-27 18:23:44 +0300

    remove shadcn drawer because it sucks

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,11 @@
-import FooterDrawer from "@/components/footer/footer-drawer/footer-drawer";
 import FooterIcons from "@/components/footer/footer-icons";
+import { Input } from "@/components/ui/input";
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 center w-full p-4 bg-secondary flex justify-between border-t border-foreground/20">
       <FooterIcons />
-      <FooterDrawer />
+      <Input placeholder="Type your message..." />
     </footer>
   );
 };

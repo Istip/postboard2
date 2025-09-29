ffa2c6b219b22aef1c593c965531fce3f795813c
Pasztor Isti <isticsek@gmail.com>
2025-09-29 18:16:56 +0300

    change title component styles

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,5 +1,5 @@
 const Title = ({ children }: { children: string }) => {
-  return <h1 className="text-2xl heading text-primary">{children}</h1>;
+  return <h1 className="text-md heading text-primary">{children}</h1>;
 };
 
 export default Title;

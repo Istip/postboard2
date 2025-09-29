f643619090790f05334ce9d23e2b751e279a6ac7
Pasztor Isti <isticsek@gmail.com>
2025-09-29 21:23:30 +0300

    update dashboard layout

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,12 +1,15 @@
 import Footer from "@/components/footer/footer";
 import Navigation from "@/components/navigation/navigation";
 import { Outlet } from "react-router";
+import { heights } from "@/lib/heights";
 
 const DashboardLayout = () => {
   return (
     <>
       <Navigation />
-      <div className="px-4 py-8 min-h-[calc(100dvh-71px)] pb-[71px] pt-[71px] my-4 bg-background">
+      <div
+        className={`px-4 py-8 min-h-[calc(100dvh-${heights.navigation}px)] pb-[${heights.footer}px] pt-[${heights.footer}px] my-4 bg-background`}
+      >
         <Outlet />
       </div>
       <Footer />

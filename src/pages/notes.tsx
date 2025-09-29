abcd11c8b6c96690bf51b58952b8b02ce804452e
Pasztor Isti <isticsek@gmail.com>
2025-09-29 22:33:12 +0300

    add mock notes content to test long pages

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -6,6 +6,24 @@ const Notes = () => {
   return (
     <BackgroundPage background={backgrounds.notes}>
       <Title>Notes</Title>
+      <div className="text-4xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+        vitae deleniti reprehenderit sint eligendi, tempora ad eaque error
+        expedita quisquam illo beatae unde asperiores nihil eos at officiis quos
+        sed.
+      </div>
+      <div className="text-4xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+        vitae deleniti reprehenderit sint eligendi, tempora ad eaque error
+        expedita quisquam illo beatae unde asperiores nihil eos at officiis quos
+        sed.
+      </div>
+      <div className="text-4xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+        vitae deleniti reprehenderit sint eligendi, tempora ad eaque error
+        expedita quisquam illo beatae unde asperiores nihil eos at officiis quos
+        sed.
+      </div>
     </BackgroundPage>
   );
 };

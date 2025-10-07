6f9933e1a24fa2154cad6f28788a3bb4643dd4cb
Pasztor Isti <isticsek@gmail.com>
2025-10-07 23:40:32 +0300

    add bigger space between title section and action buttons

--- src/components/shopping/shopping-card-components.tsx
+++ src/components/shopping/shopping-card-components.tsx
@@ -13,9 +13,9 @@ export const ShoppingCardWrapper = ({
 }) => {
   const variantStyles = {
     default:
-      "p-4 space-y-2 bg-background/50 border border-background backdrop-blur-sm rounded-lg shadow",
-    marked: "p-4 space-y-2 bg-primary border border-primary rounded-lg",
-    done: "p-4 space-y-2 bg-foreground/5 border border-foreground/30 backdrop-blur-xl rounded-lg opacity-30",
+      "p-4 space-y-4 bg-background/50 border border-background backdrop-blur-sm rounded-lg shadow",
+    marked: "p-4 space-y-4 bg-primary border border-primary rounded-lg",
+    done: "p-4 space-y-4 bg-foreground/5 border border-foreground/30 backdrop-blur-xl rounded-lg opacity-30",
   };
 
   return (

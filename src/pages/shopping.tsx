51941f894de4e3721bdb9545a3cdfa4335ca3e9f
Pasztor Isti <isticsek@gmail.com>
2025-10-07 23:08:37 +0300

    finally we started fetching data

--- src/pages/shopping.tsx
+++ src/pages/shopping.tsx
@@ -1,11 +1,36 @@
 import BackgroundPage from "@/components/helpers/background-page";
+import ShoppingCard from "@/components/shopping/shopping-card";
 import Title from "@/components/ui/title";
 import { backgrounds } from "@/lib/backgrounds";
+import { useShoppingStore } from "@/stores/shopping.store";
+import { useEffect } from "react";
 
 const ShoppingList = () => {
+  const fetchItems = useShoppingStore((state) => state.fetchItems);
+  const items = useShoppingStore((state) => state.items);
+  const loading = useShoppingStore((state) => state.loading);
+
+  useEffect(() => {
+    fetchItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  /**
+   * NOTE: This loading state is just for demo purposes.
+   * In a real app, you might want to use a skeleton loader or some other loading indicator.
+   */
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <BackgroundPage background={backgrounds.shopping}>
-      <Title>Shopping list</Title>
+      <Title className="mb-2">Shopping list</Title>
+      {items?.length === 0 && <div>Your shopping list is empty.</div>}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+        {items?.length > 0 &&
+          items.map((item) => <ShoppingCard key={item.$id} item={item} />)}
+      </div>
     </BackgroundPage>
   );
 };

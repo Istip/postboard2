4d4a4d03f3e7906d38fbaa07245d702dfc7f6e13
Pasztor Isti <isticsek@gmail.com>
2025-10-07 21:15:25 +0300

    update title to support class names

--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -1,5 +1,13 @@
-const Title = ({ children }: { children: string }) => {
-  return <h1 className="text-md heading text-primary">{children}</h1>;
+const Title = ({
+  children,
+  className,
+}: {
+  children: string;
+  className?: string;
+}) => {
+  return (
+    <h1 className={`text-md heading text-primary ${className}`}>{children}</h1>
+  );
 };
 
 export default Title;

f6a26ec456e2e3dcb38790bf6deecde9ca83ae87
Pasztor Isti <isticsek@gmail.com>
2025-10-08 22:02:55 +0300

    update shopping page

--- src/pages/shopping.tsx
+++ src/pages/shopping.tsx
@@ -1,6 +1,7 @@
 import BackgroundPage from "@/components/helpers/background-page";
 import ShoppingCard from "@/components/shopping/shopping-card";
 import ShoppingCardSkeletons from "@/components/shopping/shopping-card-skeletons";
+import ShoppingCarousel from "@/components/shopping/shopping-carousel";
 import Title from "@/components/ui/title";
 import { backgrounds } from "@/lib/backgrounds";
 import { useShoppingStore } from "@/stores/shopping.store";
@@ -10,6 +11,8 @@ const ShoppingList = () => {
   const fetchItems = useShoppingStore((state) => state.fetchItems);
   const items = useShoppingStore((state) => state.items);
   const loading = useShoppingStore((state) => state.loading);
+
+  const markedItems = items.filter((item) => item.marked);
 
   useEffect(() => {
     fetchItems();
@@ -24,10 +27,23 @@ const ShoppingList = () => {
       ) : (
         <>
           {items?.length === 0 && <div>Your shopping list is empty.</div>}
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
-            {items?.length > 0 &&
-              items.map((item) => <ShoppingCard key={item.$id} item={item} />)}
-          </div>
+          {markedItems?.length > 0 && (
+            <div className="mb-4">
+              <h2 className="mb-2 font-semibold text-lg heading">
+                Marked items
+              </h2>
+              <ShoppingCarousel items={markedItems} />
+            </div>
+          )}
+          <>
+            <h2 className="mb-2 font-semibold text-lg heading">All items</h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+              {items?.length > 0 &&
+                items.map((item) => (
+                  <ShoppingCard key={item.$id} item={item} />
+                ))}
+            </div>
+          </>
         </>
       )}
     </BackgroundPage>

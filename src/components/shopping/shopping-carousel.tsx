fcf67e3fb303c7672c7fb654ce1563ee68a7f4e6
Pasztor Isti <isticsek@gmail.com>
2025-10-09 22:48:40 +0300

    add motion for carousel card dragging

--- src/components/shopping/shopping-carousel.tsx
+++ src/components/shopping/shopping-carousel.tsx
@@ -4,6 +4,7 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
+import { motion } from "motion/react";
 
 interface Props {
   items?: Shopping[];
@@ -12,15 +13,18 @@ interface Props {
 const ShoppingCarousel = ({ items }: Props) => {
   return (
     <Carousel opts={{ align: "start", loop: true }} className="w-full">
-      <CarouselContent className="mx-0">
+      <CarouselContent className="mx-0 py-4">
         {items?.map((item) => (
           <CarouselItem
             key={item.$id}
-            className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 flex w-full text-background pl-2"
+            className="hover:cursor-grab basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 flex w-full text-background pl-2"
           >
-            <div className="w-full h-full flex">
+            <motion.div
+              className="w-full h-full flex"
+              whileTap={{ cursor: "grabbing", scale: 0.9 }}
+            >
               <ShoppingCard item={item} />
-            </div>
+            </motion.div>
           </CarouselItem>
         ))}
       </CarouselContent>

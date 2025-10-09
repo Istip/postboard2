34be2d9db4ea88287763f1c7a7f76df36990e91c
Pasztor Isti <isticsek@gmail.com>
2025-10-09 20:08:38 +0300

    fix empty list renderings

--- src/components/shopping/shopping-views/done-items.tsx
+++ src/components/shopping/shopping-views/done-items.tsx
@@ -7,11 +7,15 @@ interface Props {
 const DoneItems = ({ items }: Props) => {
   return (
     <>
-      <h2 className="my-2 font-semibold text-lg heading">Recent items</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
-        {items?.length > 0 &&
-          items.map((item) => <ShoppingCard key={item.$id} item={item} />)}
-      </div>
+      {items.length > 0 && (
+        <>
+          <h2 className="my-2 font-semibold text-lg heading">Recent items</h2>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+            {items?.length > 0 &&
+              items.map((item) => <ShoppingCard key={item.$id} item={item} />)}
+          </div>
+        </>
+      )}
     </>
   );
 };
--- src/components/shopping/shopping-views/ready-items.tsx
+++ src/components/shopping/shopping-views/ready-items.tsx
@@ -7,11 +7,15 @@ interface Props {
 const ReadyItems = ({ items }: Props) => {
   return (
     <>
-      <h2 className="mb-2 font-semibold text-lg heading">List items</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
-        {items?.length > 0 &&
-          items.map((item) => <ShoppingCard key={item.$id} item={item} />)}
-      </div>
+      {items.length > 0 && (
+        <>
+          <h2 className="mb-2 font-semibold text-lg heading">List items</h2>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
+            {items?.length > 0 &&
+              items.map((item) => <ShoppingCard key={item.$id} item={item} />)}
+          </div>
+        </>
+      )}
     </>
   );
 };

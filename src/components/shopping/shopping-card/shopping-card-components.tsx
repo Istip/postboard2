13b5ffa3559eee6b2fe14f0be6ccef88195448a2
Pasztor Isti <isticsek@gmail.com>
2025-10-09 19:40:25 +0300

    update card styles

--- src/components/shopping/shopping-card/shopping-card-components.tsx
+++ src/components/shopping/shopping-card/shopping-card-components.tsx
@@ -15,7 +15,7 @@ export const ShoppingCardWrapper = ({
     default:
       "p-2 space-y-4 bg-background/50 border dark:border-background border-foreground/15 backdrop-blur-sm rounded-xl shadow",
     marked: "p-2 space-y-4 bg-primary border border-primary rounded-xl",
-    done: "p-2 space-y-4 bg-foreground/5 border border-dashed border-foreground/30 backdrop-blur-xl rounded-xl opacity-40",
+    done: "p-2 space-y-4 bg-foreground/20 border border-dashed border-foreground/50 backdrop-blur-3xl rounded-xl opacity-40",
   };
 
   return (

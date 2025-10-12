e772356f03150c4c8fb48965be496b1fecaf99cf
Pasztor Isti <isticsek@gmail.com>
2025-10-12 21:30:01 +0300

    update dialog

--- src/components/shopping/shopping-card-dialog.tsx
+++ src/components/shopping/shopping-card-dialog.tsx
@@ -1,14 +1,34 @@
+import { Calendar, CircleUser } from "lucide-react";
+
 interface Props {
   item: Shopping;
 }
 
 const ShoppingCardDialog = ({ item }: Props) => {
+  const list = [
+    { label: item.creator, value: "Created by", icon: <CircleUser /> },
+    {
+      label: new Date(item.$createdAt).toLocaleDateString(),
+      value: "Date",
+      icon: <Calendar />,
+    },
+  ];
+
   return (
     <>
-      <h3>{item.name}</h3>
-      <p>
-        Created by <b>{item.creator}</b>
-      </p>
+      <h3 className="text-2xl pb-4 text-center">{item.name}</h3>
+      {list.map((data) => (
+        <div
+          key={data.value}
+          className="flex items-center justify-between gap-2 space-y-1"
+        >
+          <div className="center text-left gap-2 text-sm">
+            {data.icon}
+            <span>{data.value}:</span>
+          </div>
+          <span className="text-right font-semibold text-sm">{data.label}</span>
+        </div>
+      ))}
     </>
   );
 };

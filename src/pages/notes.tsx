16c2b9aac93b238704e160a22f15fe744ed8e6c2
Pasztor Isti <isticsek@gmail.com>
2025-11-06 19:37:56 +0200

    updated notes page with new test compo

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,4 +1,5 @@
 import BackgroundPage from "@/components/helpers/background-page";
+import SortableNested from "@/components/notes/nested/nested";
 import Title from "@/components/ui/title";
 import { backgrounds } from "@/lib/backgrounds";
 
@@ -6,24 +7,7 @@ const Notes = () => {
   return (
     <BackgroundPage background={backgrounds.notes}>
       <Title>Notes</Title>
-      <div className="text-4xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
-        vitae deleniti reprehenderit sint eligendi, tempora ad eaque error
-        expedita quisquam illo beatae unde asperiores nihil eos at officiis quos
-        sed.
-      </div>
-      <div className="text-4xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
-        vitae deleniti reprehenderit sint eligendi, tempora ad eaque error
-        expedita quisquam illo beatae unde asperiores nihil eos at officiis quos
-        sed.
-      </div>
-      <div className="text-4xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
-        vitae deleniti reprehenderit sint eligendi, tempora ad eaque error
-        expedita quisquam illo beatae unde asperiores nihil eos at officiis quos
-        sed.
-      </div>
+      <SortableNested />
     </BackgroundPage>
   );
 };

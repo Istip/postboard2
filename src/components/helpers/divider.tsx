a1d1328f17428e5fa0d3dd01c6b65a61e957f00d
Pasztor Isti <isticsek@gmail.com>
2025-11-06 20:24:36 +0200

    update custom divider to support multiple variants

--- src/components/helpers/divider.tsx
+++ src/components/helpers/divider.tsx
@@ -1,9 +1,17 @@
-const Divider = ({ children }: { children?: string }) => {
+interface Props {
+  children?: string;
+  variant?: "accent" | "primary";
+}
+
+const Divider = ({ children, variant = "accent" }: Props) => {
+  const textColor =
+    variant === "accent" ? "text-accent-foreground" : "text-primary-foreground";
+
   if (children && children.length > 2) {
     return (
       <div className="center gap-2">
         <div className="h-[1px] w-full bg-secondary rounded-full my-4"></div>
-        <div className="text-[10px] text-accent-foreground">{children}</div>
+        <div className={`text-[10px] ${textColor}`}>{children}</div>
         <div className="h-[1px] w-full bg-secondary rounded-full my-4"></div>
       </div>
     );

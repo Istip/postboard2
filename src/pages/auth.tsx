ce9934ea1a66075cc7f32cb467d70f7beab12371
Pasztor Isti <isticsek@gmail.com>
2025-11-06 21:35:11 +0200

    change auth page

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -3,6 +3,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "@/components/authentication/login";
 import Register from "@/components/authentication/register";
 import Redirection from "@/components/helpers/redirection";
+import { LogIn, UserPlus } from "lucide-react";
 
 const Authentication = () => {
   const user = useAuthStore((state) => state.user);
@@ -12,22 +13,33 @@ const Authentication = () => {
   }
 
   return (
-    <Tabs defaultValue="login">
-      <TabsList className="w-full mb-6">
-        <TabsTrigger className="cursor-pointer" value="login">
-          LOGIN
-        </TabsTrigger>
-        <TabsTrigger className="cursor-pointer" value="register">
-          REGISTER
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="login">
-        <Login />
-      </TabsContent>
-      <TabsContent value="register">
-        <Register />
-      </TabsContent>
-    </Tabs>
+    <div className="grid md:grid-cols-2 gap-4 min-h-screen bg-background">
+      <div className="center text-muted bg-primary h-[33vh] md:h-auto p-4 text-center">
+        <h1 className="text-4xl font-extralight">
+          Welcome to <span>Postboard</span>
+        </h1>
+      </div>
+
+      <div className="mx-auto md:my-auto w-full max-w-md p-4 mt-0">
+        <Tabs defaultValue="login">
+          <TabsList className="w-full mb-6">
+            <TabsTrigger className="cursor-pointer" value="login">
+              <LogIn className="mr-2" size={16} />
+              Already a member
+            </TabsTrigger>
+            <TabsTrigger className="cursor-pointer" value="register">
+              <UserPlus className="mr-2" size={16} />I want to join
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Login />
+          </TabsContent>
+          <TabsContent value="register">
+            <Register />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
   );
 };
 

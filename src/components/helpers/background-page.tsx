8e4ac3fa6cf808e0901e10246880b919196ce96d
Pasztor Isti <isticsek@gmail.com>
2025-11-26 23:08:14 +0200

    fix background overlay strech

--- src/components/helpers/background-page.tsx
+++ src/components/helpers/background-page.tsx
@@ -17,15 +17,14 @@ const BackgroundPage = ({ children, background }: BackgroundPageProps) => {
     bottom: `${heights.footer}px`,
     left: 0,
     right: 0,
-    overflow: "auto",
   };
 
   return (
     <div style={styles}>
-      <div className="relative z-10 min-h-full">
+      <div className="relative z-10 h-full flex flex-col">
         <div className="absolute inset-0 pointer-events-none bg-linear-to-b from-background/80 via-background/90 via-50% to-background" />
-        <div className="relative z-10 p-2 xl:px-0 max-w-7xl mx-auto">
-          {children}
+        <div className="relative z-10 flex-1 overflow-auto p-2 xl:px-0">
+          <div className="max-w-7xl mx-auto">{children}</div>
         </div>
       </div>
     </div>

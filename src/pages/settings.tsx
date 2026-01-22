125408439be437dd4b6bccf1cb7eed6eb3ccd91e
Pasztor Isti <isticsek@gmail.com>
2026-01-22 17:23:58 +0200

    update settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,7 +1,10 @@
+import FriendsList from "@/components/settings/friends-list";
+
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className="max-w-7xl mx-auto p-2 xl:p-0">
+      <h1>Settings go here</h1>
+      <FriendsList />
     </div>
   );
 };

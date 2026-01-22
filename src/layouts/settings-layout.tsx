c74430e539e268447e60940f28a4df43fad46baf
Pasztor Isti <isticsek@gmail.com>
2026-01-22 15:37:21 +0200

    remove test text

--- src/layouts/settings-layout.tsx
+++ src/layouts/settings-layout.tsx
@@ -13,12 +13,6 @@ const SettingsLayout = () => {
     >
       <Navigation />
       <Outlet />
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quo
-        voluptate voluptates vel quos, totam suscipit veritatis quisquam sequi
-        vitae dolorum, facere magnam. Quos, voluptatum architecto porro aut ab
-        necessitatibus!
-      </h1>
     </div>
   );
 };

4dbffa2915b69c0ad889ae8aa5ea21e0719157eb
Pasztor Isti <isticsek@gmail.com>
2026-05-22 20:25:20 +0300

    improve update compy

--- src/components/helpers/error.tsx
+++ src/components/helpers/error.tsx
@@ -1,7 +1,26 @@
+import { TriangleAlert, XIcon } from "lucide-react";
+import { useState } from "react";
+
 const Error = ({ children }: { children: string }) => {
+  const [show, setShow] = useState(true);
+
+  if (!show) return null;
+
   return (
-    <div className="bg-destructive text-sm text-stone-950 text-center p-2 border border-red-800">
-      {children}
+    <div className="bg-red-500 text-sm text-background text-center pl-4 pr-2 py-2 rounded-md">
+      <div className="center gap-4">
+        <div className="w-8 h-8 center">
+          <TriangleAlert className="text-red-900 animate-pulse" />
+        </div>
+        <div className="text-left">{children}</div>
+        <div
+          className="p-1 center cursor-pointer"
+          role="button"
+          onClick={() => setShow(false)}
+        >
+          <XIcon className="w-4 h-4" />
+        </div>
+      </div>
     </div>
   );
 };
